f8d0288906c6960fdfece8a0d192c2e48fd2e63d
SWATHI MAKKENA <swathimakkena@SWATHIs-MacBook-Pro.local>
2016-04-24 16:56:46 -0400

    Supports creating instances using a constructor

--- src/main/java/com/reflecty/creators/ReflectiveInstantiator.java
+++ src/main/java/com/reflecty/creators/ReflectiveInstantiator.java
@@ -1,12 +1,27 @@
 package com.reflecty.creators;
 
+import com.reflecty.BuildFactoryMachine;
+import com.reflecty.InstanceCreatorFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ReflectiveInstantiator {
     public <T> T instantiate(Class<T> clazz) {
         try {
-            return clazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Class: " + clazz.getName() + " does not have a default constructor");
+            return newInstanceFromConstructor(clazz.getDeclaredConstructors()[0], new BuildFactoryMachine(new InstanceCreatorFactory()));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Class: " + clazz.getName() + " does not have an appropriate constructor", e);
         }
+    }
+
+    private <T> T newInstanceFromConstructor(Constructor<?> constructor, BuildFactoryMachine buildFactoryMachine) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        return (T) constructor.newInstance(
+                Arrays.<Class<?>>asList(
+                        constructor.getParameterTypes()
+                ).stream().map(buildFactoryMachine::buildItRealWell).collect(Collectors.toList()).toArray()
+        );
     }
 }
